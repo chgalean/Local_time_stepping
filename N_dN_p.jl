f1219bbbdfb81430bce1d3c5745f4ffc293a0030
Carlos Galeano <chgaleanou@unal.edu.co>
2024-10-19 18:39:34 -0500

    Se actualizan las funciones base de velocidad y presión

---
+++ N_dN_p.jl
@@ -0,0 +1,22 @@
+function N_dN_p(chi,eta,m)
+    if m==1
+      N=0.25*(1-chi)*(1-eta);
+      dN_dchi=0.25*(eta-1);
+      dN_deta=0.25*(chi-1);
+    elseif m==2
+      N=0.25*(1+chi)*(1-eta);
+      dN_dchi=0.25*(1-eta);
+      dN_deta=-0.25*(1+chi);
+    elseif m==3
+      N=0.25*(1+chi)*(1+eta);
+      dN_dchi=0.25*(eta+1);
+      dN_deta=0.25*(chi+1);
+    elseif m==4
+      N=0.25*(1-chi)*(1+eta);
+      dN_dchi=-0.25*(1+eta);
+      dN_deta=0.25*(1-chi);
+    else
+      @assert m < 4
+    end
+    return N,dN_dchi,dN_deta
+  end
